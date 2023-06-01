80d4fbbc728d1198ae3d7566e3d23a581e977c35
Vesnin Vyacheslav <maphypro@gmail.com>
2023-06-01 19:49:54 +0700

    add router

---
+++ Client/src/Router.tsx
@@ -0,0 +1,24 @@
+import {createBrowserRouter} from "react-router-dom";
+import Catalog from "./pages/Catalog";
+import Course from "./pages/Course";
+import Learn from "./pages/Learn";
+import Teach from "./pages/Teach";
+
+const router = createBrowserRouter([
+    {
+        path: "/catalog",
+        element: <Catalog/>,
+    },
+    {
+        path: "/course",
+        element: <Course/>,
+    },
+    {
+        path: "/learn",
+        element: <Learn/>,
+    },
+    {
+        path: "/teach",
+        element: <Teach/>,
+    },
+]);
---
+++ Client/src/pages/Teach.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Teach = () => {
+    return (
+        <div>
+            Teach
+        </div>
+    );
+};
+
+export default Teach;
